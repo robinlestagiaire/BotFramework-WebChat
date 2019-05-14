386819770d29c80123f448dfb3d2f514cbdee461
BruceHaley <v-bruhal@micrsoft.com>
2019-05-14 10:21:32 +0000

    Previous version

---
+++ 14.customization-piping-to-redux/precache-manifest.3135c6b95b0d433b99d610a69957a317.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77b8854e08698dec8b9b9d92e27b826",
+    "url": "/BotFramework-WebChat/14.customization-piping-to-redux/index.html"
+  },
+  {
+    "revision": "db4f0b67d6a14a77c4bd",
+    "url": "/BotFramework-WebChat/14.customization-piping-to-redux/static/css/main.0cdabcaa.chunk.css"
+  },
+  {
+    "revision": "63e994fe8dd937e44d47",
+    "url": "/BotFramework-WebChat/14.customization-piping-to-redux/static/js/2.fc87e6bf.chunk.js"
+  },
+  {
+    "revision": "db4f0b67d6a14a77c4bd",
+    "url": "/BotFramework-WebChat/14.customization-piping-to-redux/static/js/main.f8a0cbbd.chunk.js"
+  },
+  {
+    "revision": "1bc08aee019745df324f",
+    "url": "/BotFramework-WebChat/14.customization-piping-to-redux/static/js/runtime~main.ad88a3fe.js"
+  }
+]);
---
+++ 17.chat-send-history/precache-manifest.d88e179143cef32f41a159a3032c2807.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f0498e0b2a663f58d2ab0d67a29884",
+    "url": "/BotFramework-WebChat/17.chat-send-history/index.html"
+  },
+  {
+    "revision": "bfe58113577a1b2b635f",
+    "url": "/BotFramework-WebChat/17.chat-send-history/static/css/main.c0c51a48.chunk.css"
+  },
+  {
+    "revision": "11fa80ac39f56a0fef5c",
+    "url": "/BotFramework-WebChat/17.chat-send-history/static/js/2.d046466a.chunk.js"
+  },
+  {
+    "revision": "bfe58113577a1b2b635f",
+    "url": "/BotFramework-WebChat/17.chat-send-history/static/js/main.bffbee69.chunk.js"
+  },
+  {
+    "revision": "d507518806ae0c26a0be",
+    "url": "/BotFramework-WebChat/17.chat-send-history/static/js/runtime~main.733ba5fe.js"
+  }
+]);
